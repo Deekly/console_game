9930f58d762a5e4238d115a547cd1138b70e2125
Deekly <rizemc01@gmail.com>
2018-03-19 11:13:50 +0200

    Random
    Numbers

---
+++ Random_Numbers.py
@@ -0,0 +1,13 @@
+#   Random_Numbers
+#   How you doin'? =)
+
+import random
+
+rand1 = random.randint(1, 6)
+rand2 = random.randrange(6)+1
+total = rand1 + rand2
+print(rand1, rand2, total)
+print("hahah")
+print("ahaahahah")
+
+input("Press Enter to end app, please =)")
