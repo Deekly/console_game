635346119fae4f345230ee3b03237ed5b7bbcd72
Deekly <rizemc01@gmail.com>
2018-03-21 15:23:28 +0200

    2nd game !!!
    yey !

---
+++ try_to_guess_v2.py
@@ -0,0 +1,39 @@
+#   try_to_guess
+#   coded by Deekly
+#   21.03.18
+
+#   Try to guess my number from 1 to 100
+
+import random
+
+print ("""
+                            !!! TRY TO GUESS !!!
+                            
+                        blah blah blah blah blah blah
+                        blah blah blah blah blah blah
+                        blah blah blah blah .... ....
+""")
+
+input ("\t\t\tPress enter if understood")
+
+rand = random.randint(1,100)
+
+print ("Lets start =)")
+
+count = 1
+digit = 0
+print ("Enter Sandman:")
+while digit != rand:
+    digit = int(input())
+    if digit > rand:
+        print ("Try less")
+        count += 1
+    else:
+        print ("Try more")
+        count += 1
+    
+
+print ("\t\t\t !!! YOU WIN !!!")
+print ("Your result:", count)
+
+input()
